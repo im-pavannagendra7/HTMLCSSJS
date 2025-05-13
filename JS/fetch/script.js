3f1c7a9726393917002401cc91683080d0532c9e
im-pavannagendra7 <20551a4650pavan@gmail.com>
2025-05-13 19:05:15 +0530

    update

---
+++ JS/fetch/script.js
@@ -0,0 +1,25 @@
+ function fetchData ()
+ {
+     fetch("https://official-joke-api.appspot.com/random_joke")
+        .then((res) => res.json())
+        .then((data)=>displayData(data))
+        .catch((err)=>console.log(err))
+ }
+setInterval(() =>
+{
+fetchData()
+},8000)
+ function displayData (joke)
+ {
+document.getElementById("main").textContent=''
+     const setup = document.createElement('h1');
+     setup.textContent= joke.setup
+     const punchline = document.createElement('h2')
+    punchline.textContent = joke.punchline + "😂😁";
+   document.getElementById('main').append(setup,punchline)
+ }
+
+
+
+ 
+  
